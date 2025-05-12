66b84bca523632f3b5eb608f7c67e162cab98125
HashiniBandara <hashinicbandara@gmail.com>
2025-05-12 21:35:36 +0530

    Day 16 (2) 🖊

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Loader from "../../components/loader";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 export default function ProductOverview() {
@@ -9,9 +12,29 @@ export default function ProductOverview() {
   }
 
   const [product, setProduct] = useState(null);
-  const [status, setStatus] = useState("loading");//other values: loaded, error
+  const [status, setStatus] = useState("loading"); //other values: loaded, error
+
+  useEffect(() => {
+    if (status == "loading") {
+      axios
+        .get(import.meta.env.VITE_BACKEND_URL + "/api/product/" + params.id)
+        .then((res) => {
+          console.log(res.data);
+          setProduct(res.data);
+          setStatus("loaded");
+        })
+        .catch(() => {
+          toast.error("Product is not available");
+          setStatus("error");
+        });
+    }
+  }, [status]);
 
   return (
-    <div className="w-full h-screen bg-gray-200 flex p-2">ProductOverview</div>
+    <div className="w-full h-full">
+      {status == "loading" &&  <Loader />}
+      {status == "loaded" && <div>Product Loaded</div>}
+      {status == "error" && <div>Product Not Found</div>}
+    </div>
   );
 }
