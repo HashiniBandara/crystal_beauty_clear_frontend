7b2b8d7c0bdcc9e862c9a99fa09e986efe17a4c6
HashiniBandara <hashinicbandara@gmail.com>
2025-06-18 21:23:05 +0530

    Email otp started 🖊

---
+++ src/pages/client/forgetPassword.jsx
@@ -0,0 +1,7 @@
+export default function ForgetPassword(){
+    return(
+        <div className="w-full h-full flex justify-center items-center">
+            <h1>Forget Password</h1>
+        </div>
+    )
+}
